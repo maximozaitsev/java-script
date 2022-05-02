750c6f681684c25649003a942d59f8c64b5767f1
maximozaitsev <maximozaitsev@gmail.com>
2022-05-02 15:31:13 +0300

    while, for, if

---
+++ lessons/lesson_2.js
@@ -0,0 +1,49 @@
+// console.log("1 -- " + true || true)
+// console.log("2 -- " + true || false)
+// console.log("3 -- " + false || true)
+// console.log("3 -- " + false || false)
+
+
+// console.log(true && true)
+// console.log(true && false)
+
+// let item_1 = 5;
+// let item_2 = 100;
+// let item_3 = 50;
+
+                // 20 = 10 + 5 * 2
+// if (item_1 > 3 || item_2 < 15 && item_3 == 50) {
+//     console.log("idi kushat")
+// } else {
+//     console.log("idi gotovit")
+// }
+
+            //while
+// let item_1 = 0;
+// let item_2 = 20;
+
+// while(item_1 < 20) {
+//     console.log(item_1 + " = idi kushat")
+//     item_1++
+// }
+
+
+            //for
+// for (let i = 0; i < 10; i++) {
+//     console.log(i , "= FOR" )
+// }
+
+let names = ["Kare",
+            "Alex",
+            "Andrey",
+            "Maxim",
+            "Anton",
+            "Vadim"]
+
+for (let item_1 of names) {
+
+if (item_1 == "Maxim") {
+    break;
+}
+    console.log(item_1)
+}
