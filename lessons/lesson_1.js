ddaaab827539a739d4003f96f51c8418c0ff68cd
maximozaitsev <maximozaitsev@gmail.com>
2022-05-02 15:30:03 +0300

    let, else, else if

---
+++ lessons/lesson_1.js
@@ -0,0 +1,43 @@
+let yolochka4 = 18;
+let yolochka6 = 6;
+let yokochka_name = " Zelyonaya";
+
+let result = yolochka4 + yolochka6;
+let result_2 = yolochka6 + yokochka_name;
+
+
+// console.log("result - " + yokochka4 + yolochka6);
+
+
+// console.log(result);
+// console.log(result_2);
+
+let t = true
+let f = false
+
+// console.log(t + f);
+
+// if (yolochka4 + yolochka6 == 10) {
+//     let apples = 3+5
+//     console.log("If true = " + apples)
+// } 
+// else if (f) {
+//     let milk = 2
+//     console.log("Else if = " + milk)
+// } else {
+//     let meat = 2+2
+//     console.log("If not true = " + meat)
+// }
+
+
+if (result >= 18 && result < 60) {
+        console.log("Full content")
+    } 
+    else if (result < 18) {
+        console.log("Multiki")
+    } 
+    else if (result >= 60) {
+        console.log("Kultura")
+    }else {
+        console.log("Good luck!")
+    }
